a0394ededae61776e7e1f89dfce100b6d5d12069
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-21 15:43:44 -0600

    Add Artist#age_at_death to calculate age based on year born and died

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,8 +4,7 @@ class Artist
   include Attributable
 
   def age_at_death
+    @died.to_i - @born.to_i
   end
-
-
 
 end
--- spec/photograph_spec.rb
+++ spec/photograph_spec.rb
@@ -2,29 +2,18 @@ require './lib/photograph'
 
 
 RSpec.describe Photograph do
-  # let () {}
+  let (:attributes) {{
+  id: "1",
+  name: "Rue Mouffetard, Paris (Boy with Bottles)",
+  artist_id: "4",
+  year: "1954"}}
+  let (:photograph) {Photograph.new(attributes)}
 
   it 'exists' do
-    attributes = {
-      id: "1",
-      name: "Rue Mouffetard, Paris (Boy with Bottles)",
-      artist_id: "4",
-      year: "1954"
-    }
-    photograph = Photograph.new(attributes)
-
     expect(photograph).to be_instance_of Photograph
   end
 
   it 'initializes with an id, name, artist id, and year' do
-    attributes = {
-      id: "1",
-      name: "Rue Mouffetard, Paris (Boy with Bottles)",
-      artist_id: "4",
-      year: "1954"
-    }
-    photograph = Photograph.new(attributes)
-
     expect(photograph.id).to eq("1")
     expect(photograph.name).to eq("Rue Mouffetard, Paris (Boy with Bottles)")
     expect(photograph.artist_id).to eq("4")
