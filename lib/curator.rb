d7815a5b8651c24e709c17caa76ec00771bc2998
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2021-12-28 12:13:33 -0600

    Add Curator#photographs_by_artist to show hash of all photos as an array for each artist as key

--- lib/curator.rb
+++ lib/curator.rb
@@ -22,4 +22,20 @@ class Curator
     artist[0]
   end
 
+  def photographs_by_artist
+    photographs_by_artist = {}
+    @photographs.each do |photo|
+      if !photographs_by_artist.has_key?(find_artist_by_id(photo.artist_id))
+        photographs_by_artist[find_artist_by_id(photo.artist_id)] = [photo]
+      elsif photographs_by_artist.has_key?(find_artist_by_id(photo.artist_id))
+        photographs_by_artist[find_artist_by_id(photo.artist_id)] << photo
+      end
+    end 
+    photographs_by_artist
+  end
+
+
+
+
+
 end
--- spec/curator_spec.rb
+++ spec/curator_spec.rb
@@ -17,6 +17,12 @@ RSpec.describe Curator do
     born: "1902",
     died: "1984",
     country: "United States"})}
+  let (:artist_3) {Artist.new({
+    id: "3",
+    name: "Diane Arbus",
+    born: "1923",
+    died: "1971",
+    country: "United States"})}
   let (:photo_1) {Photograph.new({
     id: "1",
     name: "Rue Mouffetard, Paris (Boy with Bottles)",
@@ -27,6 +33,16 @@ RSpec.describe Curator do
     name: "Moonrise, Hernandez",
     artist_id: "2",
     year: "1941"})}
+  let (:photo_3) {Photograph.new({
+    id: "3",
+    name: "Identical Twins, Roselle, New Jersey",
+    artist_id: "3",
+    year: "1967"})}
+  let (:photo_4) {Photograph.new({
+    id: "4",
+    name: "Monolith, The Face of Half Dome",
+    artist_id: "3",
+    year: "1927"})}
 
   it 'exists' do
     expect(curator).to be_instance_of Curator
@@ -53,5 +69,24 @@ RSpec.describe Curator do
 
     expect(curator.artists).to eq([artist_1, artist_2])
     expect(curator.find_artist_by_id("1")).to eq(artist_1)
+    expect(curator.find_artist_by_id("2")).to eq(artist_2)
+  end
+
+  it 'can list photographs by their respective artists' do
+    curator.add_artist(artist_1)
+    curator.add_artist(artist_2)
+    curator.add_artist(artist_3)
+    curator.add_photograph(photo_1)
+    curator.add_photograph(photo_2)
+    curator.add_photograph(photo_3)
+    curator.add_photograph(photo_4)
+
+    expected = {
+      artist_1 => [photo_1],
+      artist_2 => [photo_2],
+      artist_3 => [photo_3, photo_4]
+    }
+
+    expect(curator.photographs_by_artist).to eq(expected)
   end
 end
